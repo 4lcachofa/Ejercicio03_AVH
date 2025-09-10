2d3acb4f0a3b3d63f10ff295abe169d598a17e6b
Antonio Valdés <116698129+4lcachofa@users.noreply.github.com>
2025-09-10 08:18:41 -0600

    Update MascotasController.java

--- ejercicio03/src/main/java/com/upiiz/ejercicio03/controllers/MascotasController.java
+++ ejercicio03/src/main/java/com/upiiz/ejercicio03/controllers/MascotasController.java
@@ -31,7 +31,7 @@ public class MascotasController {
     @PostMapping("/mascotas/agregar")
     public String nuevaMascotaAgregar(@ModelAttribute("mascota") Mascota mascota) {
         mascotaService.save(mascota);
-        return "redirect:/mascotas";
+        return "redirect:/";
     }
 
     //Eliminar una mascota
@@ -43,14 +43,14 @@ public class MascotasController {
             model.addAttribute("mascota", mascota);
             return "mascotas-eliminar";
         }else{
-            return "redirect:/mascotas";
+            return "redirect:/";
         }
     }
     //2.- Eliminar la mascota y rediccionar al listado
     @PostMapping("/mascotas/eliminar")
     public String eliminarMascota (@ModelAttribute("mascota") Mascota mascota) {
         mascotaService.delete(mascota.getId());
-        return "redirect:/mascotas";
+        return "redirect:/";
     }
 
     //Actualizar una mascota
@@ -62,14 +62,15 @@ public class MascotasController {
             model.addAttribute("mascota", mascota);
             return "mascotas-actualizar";
         }else{
-            return "redirect:/mascotas";
+            return "redirect:/";
         }
     }
     //2.- Actualizar la mascota y redireccionar al listado
     @PostMapping("/mascotas/actualizar")
     public String actualizarMascota (@ModelAttribute("mascota") Mascota mascota) {
         mascotaService.update(mascota);
-        return "redirect:/mascotas";
+        return "redirect:/";
     }
 }
+
 
